49befe746272d2fc8f57dca336f68b4d70701b03
Werner van der Merwe <werner@spatialedge.ai>
2025-09-26 10:55:11 +0000

    fix: update gcloud installation script to use gpg instead of apt-key

--- src/gcloud/install.sh
+++ src/gcloud/install.sh
@@ -68,7 +68,7 @@ install_using_apt() {
     # Install dependencies
     check_packages apt-transport-https curl ca-certificates gnupg python3
     # Import key
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     apt_get_update
 
